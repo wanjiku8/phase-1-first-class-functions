f689cc008a5551ddd6130e609c0c26a87eecba80
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-02 18:36:38 +0300

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function receivesAFunction(callback) {
   // Function that returns a named function
   function returnsANamedFunction() {
     return function namedFunction() {
-      console.log("This is a named onr");
+      console.log("This is a named one");
     };
   }
   
